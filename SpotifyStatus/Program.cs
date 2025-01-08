bd5e256e821694e7da93bdcf1db34b3794205f20
Scott <scott@scottharwood.dev>
2025-01-08 22:57:57 +0000

    Add project files.

---
+++ SpotifyStatus/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Diagnostics;
+
+namespace SpotifyStatus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcessesByName("spotify");
+            string title = "";
+            foreach (var item in processes)
+            {
+                if (item.MainWindowTitle != "")
+                {
+                    title = item.MainWindowTitle;
+                    break;
+                };       
+            }
+
+            if (title == "Spotify Premium")
+            {
+                Console.Write("Not Playing");
+            } else
+            {
+                Console.Write(title);
+            }
+        }
+    }
+}
